06960d1aa752827f56004dd5c5021cde6577792f
Nikhilkathi <sonunikhil99@gmail.com>
2022-02-28 11:16:50 +0530

    okie

---
+++ javascript/one.js
@@ -0,0 +1,15 @@
+function over(){
+    document.getElementById('button_1').style.background="yellow"
+}
+function out(){
+    document.getElementById('button_1').style.background="blue"
+}
+function outer(){
+    document.getElementById('button_2').style.background="blue"
+}
+function cick(){
+    document.getElementById('button_3').style.background="yellow"
+}
+function dbck(){
+    document.getElementById('button_4').style.background="red"
+}
---
+++ two.js
@@ -0,0 +1,5 @@
+function myname(a,b){
+    return a+b;
+}
+let result= myname("rahul","gandhi");
+console.log(result);
