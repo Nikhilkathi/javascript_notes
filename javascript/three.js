477adc9e72f98da77899cbe52965d47fa5c7c604
Nikhilkathi <sonunikhil99@gmail.com>
2022-03-02 21:14:49 +0530

    okie

---
+++ javascript/three.js
@@ -0,0 +1,10 @@
+class accounts{
+    constructor(a,b,c){
+        this.customer= a
+        this.fullname= b;
+        this.address=c;
+    }
+}
+let c5= new accounts()
+const {min_Bal,Id,Name,Deposit}=require('./twothis')
+console.log
---
+++ javascript/twothis.js
@@ -0,0 +1,26 @@
+class Savings_Account{
+    min_Bal='500';
+    constructor(Id, Name, Deposit){
+        this.Id= Id;
+        this.Name= Name;
+        this.Deposit= Deposit;
+    }
+    get_Details(){
+        console.log('Your details are up-to-date');
+    }
+    get_Balance(){
+        return this.Deposit-this.min_Bal
+    }
+    Deposite(){
+        console.log('your amount has been deposited successfully');
+    }
+}
+class Update_Account extends Savings_Account{
+constructor(){
+    super(a,b,c);
+}
+}
+let c1 = new Update_Account(101,"Nikhil", 20000,'savvings coustomer','nikhilsonu', 25-2-3);
+let c2 = new Update_Account(102, "Chandu", 25000,'savvings coustomer','chandhuchinnu', 25-6-153);
+let c3 = new Update_Account(103, "Zedan", 34000,'current coustomer','zizu', 25-4-100);
+module.exports={min_Bal,Id,Name,Deposit}
