42f6f2801f0f2d0d2f9e1b78921e0f0cc9888d01
Nikhilkathi <sonunikhil99@gmail.com>
2022-03-06 23:35:48 +0530

    okie

---
+++ jstubebasic/one.js
@@ -0,0 +1,9 @@
+var fullName='Nikhil';
+var email='sonunikhil00@gmail.com';
+var password='123456';
+var conformPassword='123456';
+
+ console.log(typeof(email));
+ console.log(password);
+ console.log(conformPassword);
+ console.log(fullName);
